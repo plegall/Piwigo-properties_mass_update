726476db6fc93c5a344dc93d2ba2c277f2b525f0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-09 10:26:25 +0000

    [properties_mass_update] Update es_ES, thanks to : petaqui
    git-svn-id: http://piwigo.org/svn/extensions/properties_mass_update@15615 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos are missing in Piwigo: %s'] = '%d fotos se han perdido en Piwigo: %s';
+$lang['%d photos updated'] = '%d fotos actualizadas';
+$lang['Description for another photo'] = 'Descripción para otra foto';
+$lang['File pattern'] = 'Patrón de archivo';
+$lang['Photo description, until the end of the line'] = 'Descripción de la foto, hasta el final de la línea';
+$lang['Send File - Photo Properties Mass Update'] = 'Enviar archivo - Actualización masiva de propiedades de fotos.';
+$lang['Text file'] = 'Archivo de texto';
+$lang['Update'] = 'Actualizar';
+$lang['Wrong file, please select a plain text file'] = 'Archivo erróneo, por favor, seleccione un archivo de texto plano';
+?>
