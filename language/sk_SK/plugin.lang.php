75e18b1daf5697c5f8dcec543318927f0742a04a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-18 05:06:09 +0000

    [properties_mass_update] Update sk_SK, thanks to : xpace
    git-svn-id: http://piwigo.org/svn/extensions/properties_mass_update@15806 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos are missing in Piwigo: %s'] = '%d fotiek/fotky ch&yacute;ba/j&uacute; v Piwigo: %s';
+$lang['%d photos updated'] = '%d fotiek obnoven&yacute;ch';
+$lang['Description for another photo'] = 'Popis &#271;al&#353;ej fotky';
+$lang['File pattern'] = 'Vzor s&uacute;boru';
+$lang['Photo description, until the end of the line'] = 'Popis fotky, a&#382; po koniec riadku';
+$lang['Send File - Photo Properties Mass Update'] = 'Po&#353;li s&uacute;bor - Hromadn&aacute; obnova vlastnost&iacute; fotky';
+$lang['Text file'] = 'Textov&yacute; s&uacute;bor';
+$lang['Update'] = 'Obnovi&#357;';
+$lang['Wrong file, please select a plain text file'] = 'Nespr&aacute;vny s&uacute;bor, vyberte pros&iacute;m oby&#269;ajn&yacute; textov&yacute; s&uacute;bor';
+?>
