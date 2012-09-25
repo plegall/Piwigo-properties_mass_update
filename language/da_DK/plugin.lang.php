b9fb6ca6f973297a8f0968c9f222f82a76d9e849
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-25 21:08:41 +0000

    [properties_mass_update] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/properties_mass_update@18248 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos are missing in Piwigo: %s'] = '%d fotografier mangler i Piwigo: %s';
+$lang['%d photos updated'] = '%d fotografier opdateret';
+$lang['(comma separated)'] = '(kommasepareret)';
+$lang['Description for another photo'] = 'Beskrivelse til et andet fotografi';
+$lang['File pattern'] = 'Filmønster';
+$lang['Photo description, until the end of the line'] = 'Fotobeskrivelse, indtil slutningen af linjen';
+$lang['Send File - Photo Properties Mass Update'] = 'Send fil - Photo Properties Mass Update';
+$lang['Text file'] = 'Tekstfil';
+$lang['Update'] = 'Opdatering';
+$lang['Wrong file, please select a plain text file'] = 'Forkert fil, vælg en ren tekst-fil';
+$lang['comma'] = 'komma';
+$lang['semicolon'] = 'semikolon';
+$lang['space'] = 'mellemrum';
+$lang['tab'] = 'tabulering';
+?>
