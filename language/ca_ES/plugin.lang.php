b246f7c894baf8b914a9686f6626a3f739d76543
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-07 17:55:32 +0000

    [properties_mass_update] Update ca_ES, thanks to : josep.torne
    git-svn-id: http://piwigo.org/svn/extensions/properties_mass_update@18539 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos are missing in Piwigo: %s'] = '%d fotos no s\'han trobat a Piwigo: %s';
+$lang['%d photos updated'] = 'S\'han actualitzat %d fotos';
+$lang['Description for another photo'] = 'Descripció per una altra foto';
+$lang['File pattern'] = 'Patró del fitxer';
+$lang['Photo description, until the end of the line'] = 'Descripció de la foto, fins al final de la línia';
+$lang['Send File - Photo Properties Mass Update'] = 'Envia fitxer - Actualització en massa de les propietats de les fotos';
+$lang['(comma separated)'] = '(separat per comes)';
+$lang['Text file'] = 'Fitxer de text';
+$lang['Update'] = 'Actualització';
+$lang['Wrong file, please select a plain text file'] = 'Fitxer erroni, seleccioneu un fitxer de text senzill';
+$lang['comma'] = 'coma';
+$lang['semicolon'] = 'punt i coma';
+$lang['space'] = 'espai';
+$lang['tab'] = 'tabulació';
+?>
