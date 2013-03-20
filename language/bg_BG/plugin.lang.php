540c75db94843b2696b0d9a3f10c73f2341b979f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-20 12:42:04 +0000

    [properties_mass_update] Add bg_BG, thanks to : picsvet
    git-svn-id: http://piwigo.org/svn/extensions/properties_mass_update@21741 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Send File - Photo Properties Mass Update'] = 'Изпрати файл - Функции на снимките Обновяване изцяло';
+$lang['Wrong file, please select a plain text file'] = 'Грешен файл, моля изберете обикновен текстов файл';
+$lang['Photo description, until the end of the line'] = 'Описание на снимката, до края на линията';
+$lang['Description for another photo'] = 'Описание друга снимка';
+$lang['(comma separated)'] = '(разделени със запетая)';
+$lang['%d photos are missing in Piwigo: %s'] = '%d снимки липсват в Piwigo: %s';
+$lang['comma'] = 'запетая';
+$lang['Text file'] = 'Текстови файл';
+$lang['Update'] = 'Актуализация';
+$lang['semicolon'] = '
+точка и запетая';
+$lang['space'] = 'пространство';
+$lang['tab'] = 'таблица';
+$lang['File pattern'] = 'Файл модел';
+$lang['%d photos updated'] = '%dснимки обновяване';
+?>
