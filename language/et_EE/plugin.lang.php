6faa72358bf88b52d400e6248cf1f915cecd59b2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-10 20:50:33 +0000

    [properties_mass_update] Add et_EE, thanks to : Eggy
    git-svn-id: http://piwigo.org/svn/extensions/properties_mass_update@24852 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos are missing in Piwigo: %s'] = '%d Järgmised pildid on Piwigos kadunud: %s';
+$lang['%d photos updated'] = '%d pildid uuendatud';
+$lang['(comma separated)'] = '(koma-eraldus)';
+$lang['Description for another photo'] = 'Teise pildi kirjeldus';
+$lang['File pattern'] = 'Failimuster';
+$lang['Photo description, until the end of the line'] = 'Foto kirjeldus rea lõpuni';
+$lang['Send File - Photo Properties Mass Update'] = 'Saada fail - Foto atribuutide mass-uuendamine';
+$lang['Text file'] = 'Tekstifail';
+$lang['Update'] = 'Uuenda';
+$lang['Wrong file, please select a plain text file'] = 'Vale fail, palun valige tekstifail';
+$lang['comma'] = 'koma';
+$lang['semicolon'] = 'semokoolon';
+$lang['space'] = 'tühik';
+$lang['tab'] = 'tabulaator';
+?>
