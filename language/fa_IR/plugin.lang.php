f9024fbef00121eb1864608e0d8d83f353fe80ac
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-02-12 06:32:33 +0000

    [properties_mass_update] Update fa_IR, thanks to : farhang321
    git-svn-id: http://piwigo.org/svn/extensions/properties_mass_update@27338 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos are missing in Piwigo: %s'] = 'تعداد %d تصوير در Piwigi %s از دست داده مي‌شوند';
+$lang['%d photos updated'] = '%d تصوير بروزرساني شدند';
+$lang['(comma separated)'] = '(جدا شده با ويرگول)';
+$lang['Description for another photo'] = 'توصيف براي يك تصوير ديگر';
+$lang['File pattern'] = 'الگوي فايل';
+$lang['Photo description, until the end of the line'] = 'توصيف تصوير، تا انتهاي خط';
+$lang['Send File - Photo Properties Mass Update'] = 'ارسال فايل - بروزرساني گروهي خصوصيات تصويرها';
+$lang['Text file'] = 'فايل متني';
+$lang['Update'] = 'بروزرساني';
+$lang['Wrong file, please select a plain text file'] = 'فايل اشتباه، لطفا يك فايل متني انتخاب كنيد';
+$lang['comma'] = 'ويرگول';
+$lang['semicolon'] = 'نقطه ويرگول';
+$lang['space'] = 'فاصله';
+$lang['tab'] = 'جدول بندي';
+?>
