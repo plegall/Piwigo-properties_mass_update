5de722a5eea4f7458800762628b22a1c7b2f0f46
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-07-19 14:04:11 +0200

    Add sl_SI, thanks to MarkoAm

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Wrong file, please select a plain text file'] = 'Napačna datoteka, prosimo, izberite navadno tekstovno datoteko';
+$lang['comma'] = 'vejica';
+$lang['semicolon'] = 'podpičje';
+$lang['space'] = 'presledek';
+$lang['tab'] = 'tabular';
+$lang['File pattern'] = 'Vzorec datoteke';
+$lang['Photo description, until the end of the line'] = 'Opis fotografije, do konca vrstice';
+$lang['Send File - Photo Properties Mass Update'] = 'Pošlji datoteko - Lastnosti fotografije Masovna posodobitev';
+$lang['Text file'] = 'Tekstovna datoteka';
+$lang['Update'] = 'Posodobi';
+$lang['Description for another photo'] = 'Opis za drugo sliko';
+$lang['(comma separated)'] = '(ločeno z vejico)';
+$lang['%d photos updated'] = '%d slik posodobljenih';
+$lang['%d photos are missing in Piwigo: %s'] = '%d slik manjka v Piwigo: %s';
