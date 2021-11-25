d8f73e3cd132735666f368b9ffedd4eeba5050d2
plegall <plg@piwigo.org>
2021-11-25 15:24:08 +0100

    compatibility Piwigo 12

--- main.inc.php
+++ main.inc.php
@@ -6,6 +6,7 @@ Description: Update many photo properties at once
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=630
 Author: plg
 Author URI: http://piwigo.org
+Has Settings: true
 */
 
 if (!defined('PHPWG_ROOT_PATH'))
@@ -14,21 +15,4 @@ if (!defined('PHPWG_ROOT_PATH'))
 }
 
 define('PPMU_PATH', PHPWG_PLUGINS_PATH.basename(dirname(__FILE__)).'/');
-
-/* Plugin admin */
-add_event_handler('get_admin_plugin_menu_links', 'ppmu_admin_menu');
-function ppmu_admin_menu($menu)
-{
-  global $page;
-  
-  array_push(
-    $menu,
-    array(
-      'NAME' => 'Properties Mass Update',
-      'URL'  => get_root_url().'admin.php?page=plugin-properties_mass_update'
-      )
-    );
-
-  return $menu;
-}
 ?>
