f8910aa6ae61189bc7e2f94475b3db3fccecabef
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-16 06:22:39 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d photos are missing in Piwigo: %s'] = '%d myndir vantar í Piwigo: %s';
+$lang['%d photos updated'] = '
+%d myndir uppfærðar';
+$lang['(comma separated)'] = '(aðgreint með kommu)';
+$lang['Description for another photo'] = 'Lýsing fyrir aðra mynd';
+$lang['File pattern'] = 'Skráarmynstur';
+$lang['Photo description, until the end of the line'] = 'Myndlýsing, allt til enda línunnar';
+$lang['Send File - Photo Properties Mass Update'] = 'Senda skrá - Fjöldauppfærsla myndaeiginleika';
+$lang['Text file'] = 'Textaskrá';
+$lang['Update'] = 'Uppfæra';
+$lang['Wrong file, please select a plain text file'] = 'Röng skrá, vinsamlega veldu einfalda textaskrá';
+$lang['comma'] = 'komma';
+$lang['semicolon'] = 'semíkomma';
+$lang['space'] = 'bil';
+$lang['tab'] = 'töflugerð';
