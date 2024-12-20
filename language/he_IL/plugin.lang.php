f6bab7d44811bc7566c55f199de9610a90957759
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:16:15 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,21 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d photos are missing in Piwigo: %s'] = '%d תמונות חסרות ב-Piwigo: %s';
+$lang['%d photos updated'] = '%d תמונות עודכנו';
+$lang['(comma separated)'] = '(מופרדות בפסיק)';
+$lang['Description for another photo'] = 'תיאור לתמונה נוספת';
+$lang['File pattern'] = 'תבנית קובץ';
+$lang['Photo description, until the end of the line'] = 'תיאור תמונה, עד סוף השורה';
+$lang['Send File - Photo Properties Mass Update'] = 'שלח קובץ - עדכון מאפייני תמונה מרובים';
+$lang['Text file'] = 'קובץ טקסט';
+$lang['Update'] = 'עדכן';
+$lang['Wrong file, please select a plain text file'] = 'קובץ שגוי, אנא בחר קובץ טקסט פשוט';
+$lang['comma'] = 'פסיק';
+$lang['semicolon'] = 'נקודה פסיל';
+$lang['space'] = 'רווח';
+$lang['tab'] = 'טאבולציה';
